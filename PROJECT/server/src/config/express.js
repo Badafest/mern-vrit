74c0c95bff7af3573ec0ef09bfc51c40e5720e4c
Sandip Dahal <47387625+Badafest@users.noreply.github.com>
2023-10-29 11:43:31 +0545

    fix cors issue

--- PROJECT/server/src/config/express.js
+++ PROJECT/server/src/config/express.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json({ limit: "510kb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({}));
+// app.use(cors({}));
 
 app.use(
   cors({
